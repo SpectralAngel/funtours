25f268a7d3d5c0b8e5a6da7b9c09824805813148
Carlos Flores <cafg10@gmail.com>
2015-06-21 23:49:14 -0600

    Porting template to WordPress

---
+++ front-page.php
@@ -0,0 +1,334 @@
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+	<?php
+	wp_enqueue_style( 'fun-tours-grid-css', get_template_directory_uri() . '/template/css/grid.css' );
+	wp_enqueue_style( 'sytle-css', get_template_directory_uri() . '/template/css/style.css' );
+	wp_enqueue_style( 'camera-css', get_template_directory_uri() . '/template/css/camera.css' );
+	wp_enqueue_style( 'fancybox-css', get_template_directory_uri() . '/template/css/jquery.fancybox.css' );
+	wp_enqueue_style( 'booking-css', get_template_directory_uri() . '/template/css/booking.css' );
+	?>
+	<?php wp_head(); ?>
+	<style type="text/css">
+		html {
+			margin-top: 0 !important;
+		}
+	</style>
+</head>
+<body>
+<div class="page">
+	<!--
+	  ========================================================
+								  HEADER
+	  ========================================================
+	  -->
+	<header>
+		<div class="header_panel">
+			<div class="container">
+				<div class="brand">
+					<h1 class="brand_name"><a href="./">Travel</a></h1>
+
+					<p class="brand_slogan">Guide</p>
+				</div>
+				<div class="booking_wr">
+					<form id="bookingForm" class="booking-form">
+						<h2 class="center">Best places to stay</h2>
+						<h4 class="center">Explore the planet</h4>
+
+						<div class="tmInput">
+							<input name="Name" placeHolder="Name" type="text"
+							       data-constraints="@NotEmpty @Required @AlphaSpecial">
+						</div>
+						<div class="tmInput">
+							<input name="E-mail" placeHolder="Your E-mail"
+							       type="text"
+							       data-constraints="@NotEmpty @Required @Email">
+						</div>
+						<div class="tmInput">
+							<input name="Message" placeHolder="Your Message"
+							       type="text"
+							       data-constraints="@NotEmpty @Required">
+						</div>
+						<a href="#" data-type="submit">Get the best offers</a>
+
+						<p>
+							<small>* Ceplicaboserde neraflorem ipsumoditauteras
+								Monsequngni doloreptate sequsede
+							</small>
+						</p>
+					</form>
+				</div>
+			</div>
+		</div>
+		<div class="camera_container">
+			<div id="camera" class="camera_wrap">
+				<div
+					data-src="<?php bloginfo('template_directory'); ?>/template/images/page-1_slide01.jpg">
+					<div class="camera_caption fadeIn"></div>
+				</div>
+				<div
+					data-src="<?php bloginfo('template_directory'); ?>/template/images/page-1_slide02.jpg">
+					<div class="camera_caption fadeIn"></div>
+				</div>
+				<div
+					data-src="<?php bloginfo('template_directory'); ?>/template/images/page-1_slide03.jpg">
+					<div class="camera_caption fadeIn"></div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!--
+	  ========================================================
+								  CONTENT
+	  ========================================================
+	  -->
+	<main>
+		<section class="well bg-color1">
+			<div class="container">
+				<h2 class="center">Let's enjoy the wonders of the<br/>nature
+					together!</h2>
+
+				<p class="txt1 off1 center">Csenim admin sciporiosam nasaer.
+					Veasisi ut aliquimonseq uatueras niuasuis autem. Maerser
+					saveler eumiure repasce nasukerdas rerertas asuis autem
+					veleumiure.</p>
+				<ul class="product-list row">
+					<li class="grid_4 wow fadeInLeft"><img
+							src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img01.jpg"
+							alt="">
+
+						<h3><a href="#">Quality Consultation</a></h3>
+
+						<p>Vaserbo nerafaes sety krscakertsvitae asaloreo qui
+							ratione voluptate kjahsaus. Asqui dolorem ipsum,
+							quia dolor.</p>
+					</li>
+					<li class="grid_4 wow fadeIn">
+						<h3><a href="#">Travel Center</a></h3>
+
+						<p>Bautsrkertyu rsvitae ertyasnemo lasec vasptaia
+							goertayse. Lerernatur auteas oditaut ratione
+							voluptate kjahsaus.</p><img
+							src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img02.jpg"
+							alt="">
+					</li>
+					<li class="grid_4 wow fadeInRight"><img
+							src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img03.jpg"
+							alt="">
+
+						<h3><a href="#">Luxury Travel</a></h3>
+
+						<p>Sety krscakertsvitae asaloreo quide ratione voluptate
+							kjahsaus. Sertuserbo dolor mierta vitrdasas.</p>
+					</li>
+				</ul>
+				<p class="txt1 center">Derase reprehenderit, qui in ea voluptate
+					velit eslertase, quam nihil molestiae consequatur. vel
+					illum, quidolorem eum fugiat, quo voluptas nulla pariatur.
+					Csenim admin sciporiosam nasaerasisi ut aliquimonses
+					sertasmiutasa kertyaesre veriuyasa uatasuis autemeumiure
+					repasce nasukerdas rerertas. </p>
+			</div>
+		</section>
+		<section class="well2">
+			<div class="container">
+				<h2 class="center">News & events</h2>
+
+				<p class="txt1 center">Penepre henderitui in ea voluptate velit
+					eslertase quam nihil molestiae consequatur.</p>
+
+				<div class="row off2">
+					<article class="grid_6 wow fadeInRight">
+						<div class="row">
+							<div class="grid_3"><img
+									src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img04.jpg"
+									alt="" class="mg-add"></div>
+							<div class="grid_3">
+								<time datetime="2015-10-13">13/10</time>
+								<h5 class="primary"><a href="#">Cenaserleceas
+										strices phal edaty fenanec sit aser
+										rment lement velursu.</a></h5>
+
+								<p>Ut ts dolor apegement velu rsusen elnean ctor
+									wisetaurna aserta. Aliam eratvpa miuyas tis
+									ac turpis. </p>
+							</div>
+						</div>
+					</article>
+					<article data-wow-delay=".2s" class="grid_6 wow fadeInRight">
+						<div class="row">
+							<div class="grid_3"><img
+									src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img05.jpg"
+									alt="" class="mg-add"></div>
+							<div class="grid_3">
+								<time datetime="2015-10-12">12/10</time>
+								<h5 class="primary"><a href="#">Leugiata
+										lesrleceas ctrasi ices phaledaty fenanec
+										sitets aser erment dopegete.</a></h5>
+
+								<p>Moleacene anrit meases dey uam socis natoqu
+									eagnis dimte dulmuese feugiata lesuerleceas
+									strices phaledaty.</p>
+							</div>
+						</div>
+					</article>
+				</div>
+				<div class="row">
+					<article data-wow-delay=".4s"
+					         class="grid_6 wow fadeInRight">
+						<div class="row">
+							<div class="grid_3"><img
+									src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img06.jpg"
+									alt="" class="mg-add"></div>
+							<div class="grid_3">
+								<time datetime="2015-10-11">11/10</time>
+								<h5 class="primary"><a href="#">Senanec sit aser
+										erment dolor apegete. Cuyras esuerleceas
+										strices phaledaty.</a></h5>
+
+								<p>Sepegement velu miutasase rsusen elnean ctor
+									wisetaurna aserta. Aliam eratvpa miuyas tis
+									ac turpis. Cinteger rutrum kertyua
+									delertante.</p>
+							</div>
+						</div>
+					</article>
+					<article data-wow-delay=".6s"
+					         class="grid_6 wow fadeInRight">
+						<div class="row">
+							<div class="grid_3"><img
+									src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img07.jpg"
+									alt="" class="mg-add"></div>
+							<div class="grid_3">
+								<time datetime="2015-10-10">10/10</time>
+								<h5 class="primary"><a href="#">Dolouyts ta
+										lesuerle serceas strices phaledaty
+										fenanec site aser erment dolose.</a>
+								</h5>
+
+								<p>Veases dey vileacene anritma uam socis natoqu
+									eagnis dimte dulmuese feugiata lesuerleceas
+									strices phaledaty fenanec sit aser
+									erment. </p>
+							</div>
+						</div>
+					</article>
+				</div>
+			</div>
+		</section>
+		<section class="gallery">
+			<a href="<?php bloginfo('template_directory'); ?>/template/images/page-1_img08_original.jpg" data-fancybox-group="1" class="thumb">
+				<img src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img08.jpg" alt="">
+				<span class="thumb_overlay"></span><span class="thumb_overlay_cnt">sauntmagni dolores</span>
+			</a>
+			<a href="<?php bloginfo('template_directory'); ?>/template/images/page-1_img09_original.jpg" data-fancybox-group="1" class="thumb">
+				<img src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img09.jpg" alt="">
+				<span class="thumb_overlay"></span><span class="thumb_overlay_cnt">lement velursu</span>
+			</a>
+			<a href="<?php bloginfo('template_directory'); ?>/template/images/page-1_img10_original.jpg" data-fancybox-group="1" class="thumb">
+				<img src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img10.jpg" alt="">
+				<span class="thumb_overlay"></span><span class="thumb_overlay_cnt">edaty fenanec</span>
+			</a>
+			<a href="<?php bloginfo('template_directory'); ?>/template/images/page-1_img11_original.jpg" data-fancybox-group="1" class="thumb">
+				<img src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img11.jpg" alt="">
+				<span class="thumb_overlay"></span><span class="thumb_overlay_cnt">sauntmagni dolores</span>
+			</a>
+			<a href="<?php bloginfo('template_directory'); ?>/template/images/page-1_img12_original.jpg" data-fancybox-group="1" class="thumb">
+				<img src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img12.jpg" alt="">
+				<span class="thumb_overlay"></span><span class="thumb_overlay_cnt">lement velursu</span>
+			</a>
+			<a href="<?php bloginfo('template_directory'); ?>/template/images/page-1_img13_original.jpg" data-fancybox-group="1" class="thumb">
+				<img src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img13.jpg" alt="">
+				<span class="thumb_overlay"> </span><span class="thumb_overlay_cnt">sauntmagni dolores</span>
+			</a>
+		</section>
+		<section class="well ins1">
+			<div class="container">
+				<h2 class="center">Top Destinations</h2>
+				<p class="txt1 center">Selum quidolorem eum fugiatuo voluptas
+					nulla parenim admin sciporiosam nasaerasi.</p>
+				<ul class="pricing-table row">
+					<li data-equal-group="1" data-wow-delay=".3s" class="grid_3 wow fadeInLeft">
+						<div>
+						<img src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img14.jpg" alt="">
+						<div class="price">$987.<sup>00</sup></div>
+						<p>Beciegast nveriti vesasety ertya aset aplicabr
+							rtyasnemaso eniptaiades.</p>
+						<a href="#" class="link">More</a>
+						</div>
+					</li>
+					<li data-equal-group="1" class="grid_3 wow fadeInLeft">
+						<div>
+						<img src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img15.jpg" alt="">
+
+						<div class="price">$1240.<sup>00</sup></div>
+						<p>Ntreasas kuyrtasacuytrsas leyrtert muyaseas tas bytrasade vtreasa.</p>
+						<a href="#" class="link">More</a>
+						</div>
+					</li>
+					<li data-equal-group="1" class="grid_3 wow fadeInRight">
+						<img src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img16.jpg" alt="">
+
+						<div class="price">$895.<sup>00</sup></div>
+						<p>Beciegast nveresasety dertya aset aplicabrd yasnesemaso eniptaiades.</p>
+						<a href="#" class="link">More</a>
+					</li>
+					<li data-equal-group="1" data-wow-delay=".3s"
+					    class="grid_3 wow fadeInRight">
+						<img src="<?php bloginfo('template_directory'); ?>/template/images/page-1_img17.jpg" alt="">
+
+						<div class="price">$999.<sup>99</sup></div>
+						<p>Ntreasas kuyrtasadcuytrsas leyrterte muyases
+							litaytrade vtreasa.</p>
+						<a href="#" class="link">More</a>
+					</li>
+				</ul>
+			</div>
+		</section>
+		<section data-url="<?php bloginfo('template_directory'); ?>/template/images/parallax1.jpg" data-mobile="true" class="well3 center parallax">
+			<h2>Luxury travel</h2>
+			<h3>Just got a little bit cheaper</h3>
+			<a href="#" class="btn">More</a>
+		</section>
+	</main>
+	<!--
+	  ========================================================
+								  FOOTER
+	  ========================================================
+	  -->
+	<footer class="mobile-center">
+		<div class="container">
+			<div class="row">
+				<div class="grid_4">
+					<ul class="inline-list">
+						<li><a href="#" class="fa-twitter"></a></li>
+						<li><a href="#" class="fa-google-plus"></a></li>
+						<li><a href="#" class="fa-facebook"></a></li>
+						<li><a href="#" class="fa-rss"></a></li>
+					</ul>
+				</div>
+				<div class="grid_3">
+					<address>28 Jackson Blvd Ste 1020 Chicago<br/> IL 60604-2340
+					</address>
+				</div>
+				<div class="grid_5"><a href="mailto:#" class="fa-envelope">info@demolink.org</a>
+
+					<div class="copyright"> © <span id="copyright-year"></span>
+						| All rights reserved
+					</div>
+					<!-- {%FOOTER_LINK} -->
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
+<script
+	src="<?php bloginfo('template_directory'); ?>/template/js/script.js"></script>
+</body>
+</html>
